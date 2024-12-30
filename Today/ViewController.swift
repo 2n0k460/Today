a7181b33b75c12e6565176fd849f9ce46fd73679
2n0k460 <tunokabo@gmail.com>
2024-12-30 21:26:48 +0900

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by 麻原彪史 on 2024/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
