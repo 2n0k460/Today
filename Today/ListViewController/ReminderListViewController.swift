4c9e33e4d5854b2fd8e457cb250c82944cb7e23b
2n0k460 <tunokabo@gmail.com>
2025-01-01 20:09:50 +0900

    セクション2-2

--- Today/ListViewController/ReminderListViewController.swift
+++ Today/ListViewController/ReminderListViewController.swift
@@ -1,45 +1,37 @@
-//
-//  ViewController.swift
-//  Today
-//
-//  Created by 麻原彪史 on 2024/12/30.
-//
-
 import UIKit
 
+
 class ReminderListViewController: UICollectionViewController {
-    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
-    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
-    
     var dataSource: DataSource!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
-        
-        let cellRegistration = UICollectionView.CellRegistration {
-            (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
-            let reminder = Reminder.sampleData[indexPath.item]
-            var contentConfiguration = cell.defaultContentConfiguration()
-            contentConfiguration.text = reminder.title
-            cell.contentConfiguration = contentConfiguration
-        }
-        
+
+
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+
+
         dataSource = DataSource(collectionView: collectionView) {
             (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
             return collectionView.dequeueConfiguredReusableCell(
                 using: cellRegistration, for: indexPath, item: itemIdentifier)
         }
-        
+
+
         var snapshot = Snapshot()
         snapshot.appendSections([0])
         snapshot.appendItems(Reminder.sampleData.map { $0.title })
         dataSource.apply(snapshot)
-        
+
+
         collectionView.dataSource = dataSource
     }
+
 
     private func listLayout() -> UICollectionViewCompositionalLayout {
         var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
@@ -48,4 +40,3 @@ class ReminderListViewController: UICollectionViewController {
         return UICollectionViewCompositionalLayout.list(using: listConfiguration)
     }
 }
-
