649d49c0c83b1e0f7eea5379144be4fd2ac3e4f7
2n0k460 <tunokabo@gmail.com>
2025-01-02 22:09:43 +0900

    セクション3-4

--- Today/ListViewController/ReminderListViewController+DataSource.swift
+++ Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -1,11 +1,13 @@
 import UIKit
 
-
 extension ReminderListViewController {
     typealias DataSource = UICollectionViewDiffableDataSource<Int, Reminder.ID>
     typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Reminder.ID>
-    
-    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
+
+    func cellRegistrationHandler(
+        cell:
+            UICollectionViewListCell, indexPath: IndexPath, id: Reminder.ID
+    ) {
         let reminder = reminder(withId: id)
         var contentConfiguration = cell.defaultContentConfiguration()
         contentConfiguration.text = reminder.title
@@ -13,28 +15,28 @@ extension ReminderListViewController {
         contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(
             forTextStyle: .caption1)
         cell.contentConfiguration = contentConfiguration
-        
+
         var doneButtonConfiguration = doneButtonConfiguration(for: reminder)
         doneButtonConfiguration.tintColor = .todayListCellDoneButtonTint
         cell.accessories = [
             .customView(configuration: doneButtonConfiguration), .disclosureIndicator(displayed: .always)
         ]
-        
+
         var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
         backgroundConfiguration.backgroundColor = .todayListCellBackground
         cell.backgroundConfiguration = backgroundConfiguration
     }
-    
+
     func reminder(withId id: Reminder.ID) -> Reminder {
         let index = reminders.indexOfReminder(withId: id)
         return reminders[index]
     }
-    
+
     func updateReminder(_ reminder: Reminder) {
-         let index = reminders.indexOfReminder(withId: reminder.id)
-         reminders[index] = reminder
-     }
-    
+        let index = reminders.indexOfReminder(withId: reminder.id)
+        reminders[index] = reminder
+    }
+
     func completeReminder(withId id: Reminder.ID) {
         var reminder = reminder(withId: id)
         reminder.isComplete.toggle()
@@ -47,10 +49,10 @@ extension ReminderListViewController {
         let symbolName = reminder.isComplete ? "circle.fill" : "circle"
         let symbolConfiguration = UIImage.SymbolConfiguration(textStyle: .title1)
         let image = UIImage(systemName: symbolName, withConfiguration: symbolConfiguration)
-        let button = UIButton()
+        let button = ReminderDoneButton()
+        button.id = reminder.id
         button.setImage(image, for: .normal)
         return UICellAccessory.CustomViewConfiguration(
             customView: button, placement: .leading(displayed: .always))
     }
 }
-
