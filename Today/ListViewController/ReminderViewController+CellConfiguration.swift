f16b71597d92769785b0848e0bd8d6927e10f600
2n0k460 <tunokabo@gmail.com>
2025-01-04 23:13:21 +0900

    セクション4-1

--- Today/ListViewController/ReminderViewController+CellConfiguration.swift
+++ Today/ListViewController/ReminderViewController+CellConfiguration.swift
@@ -2,4 +2,13 @@ import UIKit
 
 
 extension ReminderViewController {
+    func defaultConfiguration(for cell: UICollectionViewListCell, at row: Row)
+    -> UIListContentConfiguration
+    {
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = text(for: row)
+        contentConfiguration.textProperties.font = UIFont.preferredFont(forTextStyle: row.textStyle)
+        contentConfiguration.image = row.image
+        return contentConfiguration
+    }
 }
