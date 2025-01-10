5b037166cf94fa5eadae483666f755fbae909137
2n0k460 <tunokabo@gmail.com>
2025-01-10 22:27:27 +0900

    セクション6-6

--- Today/ContentViews/TextFieldContentView.swift
+++ Today/ContentViews/TextFieldContentView.swift
@@ -1,10 +1,9 @@
 import UIKit
 
-
 class TextFieldContentView: UIView, UIContentView {
     struct Configuration: UIContentConfiguration {
         var text: String? = ""
-        
+
         func makeContentView() -> UIView & UIContentView {
             return TextFieldContentView(self)
         }
@@ -16,12 +15,11 @@ class TextFieldContentView: UIView, UIContentView {
             configure(configuration: configuration)
         }
     }
-    
+
     override var intrinsicContentSize: CGSize {
         CGSize(width: 0, height: 44)
     }
-    
-    
+
     init(_ configuration: UIContentConfiguration) {
         self.configuration = configuration
         super.init(frame: .zero)
@@ -29,19 +27,18 @@ class TextFieldContentView: UIView, UIContentView {
         textField.clearButtonMode = .whileEditing
     }
 
-
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     func configure(configuration: UIContentConfiguration) {
         guard let configuration = configuration as? Configuration else { return }
         textField.text = configuration.text
     }
-    
-    extension UICollectionViewListCell {
-        func textFieldConfiguration() -> TextFieldContentView.Configuration {
-            TextFieldContentView.Configuration()
-        }
+}
+
+extension UICollectionViewListCell {
+    func textFieldConfiguration() -> TextFieldContentView.Configuration {
+        TextFieldContentView.Configuration()
     }
 }
--- Today/ListViewController/ReminderViewController+Row.swift
+++ Today/ListViewController/ReminderViewController+Row.swift
@@ -8,6 +8,7 @@ extension ReminderViewController {
         case notes
         case time
         case title
+        case editableText(String)
         
         var imageName: String? {
             switch self {
