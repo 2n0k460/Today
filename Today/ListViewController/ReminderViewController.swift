788ace2964bf29a88bf95d77ed80dcd811d2f631
2n0k460 <tunokabo@gmail.com>
2025-01-15 23:56:50 +0900

    セクション7-5

--- Today/ListViewController/ReminderViewController.swift
+++ Today/ListViewController/ReminderViewController.swift
@@ -69,7 +69,14 @@ class ReminderViewController: UICollectionViewController {
         cell.tintColor = .todayPrimaryTint
     }
     
+    @objc func didCancelEdit() {
+        workingReminder = reminder
+        setEditing(false, animated: true)
+    }
+    
     private func prepareForEditing() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .cancel, target: self, action: #selector(didCancelEdit))
         updateSnapshotForEditing()
     }
 
@@ -86,6 +93,7 @@ class ReminderViewController: UICollectionViewController {
     }
     
     private func prepareForViewing() {
+        navigationItem.leftBarButtonItem = nil
         if workingReminder != reminder {
             reminder = workingReminder
         }
